12815f9366400cbcfdab88538ff9bacc31e2f4ea
An7orAhmed <antor.mee@gmail.com>
2024-11-12 21:03:33 +0600

    title changed

--- src/DragDrop/DragComponent.jsx
+++ src/DragDrop/DragComponent.jsx
@@ -19,8 +19,8 @@ export default function DragComponent() {
   return (
     <div className="bg-white shadow-[0_100px_100px_30px_rgba(0,0,0,0.1)] rounded-lg w-1/3 px-5 pt-3 pb-5">
       <div className="flex justify-between items-center pb-[8px] border-b border-[#e0e0e0]">
-        <h2 className="text-black text-[17px] font-[600]">
-          Drag & drop here your wallpapers
+        <h2 className="text-2xl text-slate-700 font-[600]">
+        Macbook Notch Hide Wallpaper Creator
         </h2>
         <button className={`bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded ${showBtn || 'hidden'}`}>
           Generate
