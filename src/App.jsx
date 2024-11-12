5ce7032b8bedf65bc9d21aa2d08e0d8019c83575
An7orAhmed <antor.mee@gmail.com>
2024-11-12 21:01:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import DragComponent from './DragDrop/DragComponent';
+
+function App() {
+  return (
+    <div className="flex h-screen justify-center items-center px-5 bg-slate-50">
+      <DragComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ src/DragDrop/DragComponent.jsx
@@ -0,0 +1,38 @@
+import { CustomDragDrop } from "./CustomContainer";
+import { useState } from "react";
+
+export default function DragComponent() {
+  const [ownerLicense, setOwnerLicense] = useState([]);
+  const [showBtn, setShowBtn] = useState(false);
+
+  function uploadFiles(f) {
+    setOwnerLicense([...ownerLicense, ...f]);
+    setShowBtn(true);
+  }
+
+  function deleteFile(indexImg) {
+    const updatedList = ownerLicense.filter((ele, index) => index !== indexImg);
+    setOwnerLicense(updatedList);
+    setShowBtn(updatedList.length > 0);
+  }
+
+  return (
+    <div className="bg-white shadow-[0_100px_100px_30px_rgba(0,0,0,0.1)] rounded-lg w-1/3 px-5 pt-3 pb-5">
+      <div className="flex justify-between items-center pb-[8px] border-b border-[#e0e0e0]">
+        <h2 className="text-black text-[17px] font-[600]">
+          Drag & drop here your wallpapers
+        </h2>
+        <button className={`bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded ${showBtn || 'hidden'}`}>
+          Generate
+        </button>
+      </div>
+      <CustomDragDrop
+        ownerLicense={ownerLicense}
+        onUpload={uploadFiles}
+        onDelete={deleteFile}
+        count={5}
+        formats={["jpg", "jpeg", "png"]}
+      />
+    </div>
+  );
+}
